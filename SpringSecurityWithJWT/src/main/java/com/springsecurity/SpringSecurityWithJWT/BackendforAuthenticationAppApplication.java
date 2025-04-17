2621edec987716ca18b9b1c8d02d78562a6b3b5c
kaushal <kaushalvadi7777@gmail.com>
2025-04-17 22:04:44 +0530

    Initial setup for Authentication app

---
+++ SpringSecurityWithJWT/src/main/java/com/springsecurity/SpringSecurityWithJWT/BackendforAuthenticationAppApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.SpringSecurityWithJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendforAuthenticationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendforAuthenticationAppApplication.class, args);
+	}
+
+}
